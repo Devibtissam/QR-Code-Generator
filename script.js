b88834c32859fa8bdfcf3645a669ddab541db1d3
Devibtissam <dev.ibtissamelhammoumy@gmail.com>
2022-09-12 23:56:10 +0100

    select input fields value

--- script.js
+++ script.js
@@ -2,10 +2,27 @@ const generateBtn = document.querySelector('[data-generate-qr-btn]');
 const editBtn = document.querySelector('[data-go-back-btn]');
 const flippedCards = document.querySelectorAll('section');
 const url = document.querySelector('[data-url]');
+const mainColor = document.querySelector('input#main-color');
+const bgColor = document.querySelector('input#background-color');
+const size = document.querySelector('input#size');
+const margin = document.querySelector('input#margin');
+const format = document.querySelectorAll('input[type=checkbox]');
+
 const btns = [generateBtn, editBtn];
 
 
+// GET value of  checked chechbox of image format
+function getFromatValue(checkboxes){
+    checkboxes.forEach(checkbox => {
+        if(checkbox.checked){
+            console.log(checkbox.value);
+            return checkbox.value;
+        }
+    });
+}
 
+
+// check that url not emply
 function validUrl(url) {
   if (url.value.length >= 1) {
     generateBtn.disabled = false;
@@ -18,6 +35,18 @@ function validUrl(url) {
   }
 }
 
+function displayQRCode(url, mainColor, bgColor, size, margin, format){
+    const imageFormat = getFromatValue(format);
+    const urlVal = url.value;
+    const mainColorVal = mainColor.value;
+    const bgColorVal = bgColor.value;
+    const sizeVal = size.value;
+    const marginVal = margin.value;
+
+    console.log(imageFormat, urlVal, mainColorVal, bgColorVal, sizeVal, marginVal);
+
+}
+
 
 url.addEventListener('keyup', () => {
   const validURL = validUrl(url);
@@ -26,6 +55,7 @@ url.addEventListener('keyup', () => {
       btn.addEventListener('click', (e) => {
         e.preventDefault();
         flippedCards.forEach((card) => card.classList.toggle('flipped'));
+        // displayQRCode(url, mainColor, bgColor, size, margin, format);
       });
     });
   }
