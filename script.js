3dbfcf222b5b4fba95810dcdbbf043ab374bebb2
Devibtissam <dev.ibtissamelhammoumy@gmail.com>
2022-09-13 20:57:32 +0100

    generate qr code

--- script.js
+++ script.js
@@ -6,21 +6,22 @@ const mainColor = document.querySelector('input#main-color');
 const bgColor = document.querySelector('input#background-color');
 const size = document.querySelector('input#size');
 const margin = document.querySelector('input#margin');
-const format = document.querySelectorAll('input[type=checkbox]');
+const format = document.querySelectorAll('input[type=radio]');
+const QRCodeContainer = document.querySelector('.qr-image');
+const QRImg = document.querySelector('.qr-code');
+
+
 
 const btns = [generateBtn, editBtn];
 
-
 // GET value of  checked chechbox of image format
-function getFromatValue(checkboxes){
-    checkboxes.forEach(checkbox => {
-        if(checkbox.checked){
-            console.log(checkbox.value);
-            return checkbox.value;
-        }
-    });
+function getFormatValue(checkboxes) {
+  for (const checkbox of checkboxes) {
+    if (checkbox.checked) {
+      return checkbox.value;
+    }
+  }
 }
-
 
 // check that url not emply
 function validUrl(url) {
@@ -35,28 +36,58 @@ function validUrl(url) {
   }
 }
 
-function displayQRCode(url, mainColor, bgColor, size, margin, format){
-    const imageFormat = getFromatValue(format);
-    const urlVal = url.value;
-    const mainColorVal = mainColor.value;
-    const bgColorVal = bgColor.value;
-    const sizeVal = size.value;
-    const marginVal = margin.value;
+const  getParams = params => {
+ return {
+   format: params.imageFormat,
+   data: params.urlVal,
+   size: `${params.sizeVal}x${params.sizeVal}`,
+   color: params.mainColorVal.replace('#', ''),
+   bgcolor: params.bgColorVal.replace('#', ''),
+   qzone: params.marginVal,
+ };
 
-    console.log(imageFormat, urlVal, mainColorVal, bgColorVal, sizeVal, marginVal);
 
 }
 
+getQRCode = async (parameters) => {
+  const UrlParams = new URLSearchParams(parameters).toString();
+  const response = await fetch(
+    `http://api.qrserver.com/v1/create-qr-code/?${UrlParams}`
+  );
 
-url.addEventListener('keyup', () => {
-  const validURL = validUrl(url);
-  if (validURL) {
-    btns.forEach((btn) => {
-      btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        flippedCards.forEach((card) => card.classList.toggle('flipped'));
-        // displayQRCode(url, mainColor, bgColor, size, margin, format);
-      });
-    });
+  if (response.ok) {
+    return response.url;
   }
+};
+
+displayQRCode = async () => {
+  const imageFormat = getFormatValue(format);
+  const urlVal = url.value;
+  const mainColorVal = mainColor.value;
+  const bgColorVal = bgColor.value;
+  const sizeVal = size.value;
+  const marginVal = margin.value;
+
+
+  const parameters = getParams({imageFormat,urlVal,mainColorVal, bgColorVal,sizeVal,marginVal})
+  const srcImg = await getQRCode(parameters);
+  QRImg.src = srcImg;
+  QRImg.alt = "QR Code";
+  QRCodeContainer.appendChild(QRImg);
+};
+
+url.addEventListener('change', () => {
+  validUrl(url);
 });
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (btn.hasAttribute('data-generate-qr-btn')) {
+      displayQRCode();
+    }
+    flippedCards.forEach((card) => card.classList.toggle('flipped'));
+  });
+});
+
+
